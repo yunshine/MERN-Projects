46ee8500a8f544238d10f9ef8a2a4cda447e326d
Yun Chung <yun.chung@mac.com>
2021-08-18 17:52:40 +0900

    create route

--- mern-todo/client/src/App.js
+++ mern-todo/client/src/App.js
@@ -1,11 +1,43 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-        <p>MERN Todo</p>
-    </div>
-  );
+    return (
+        <Router>
+            <Switch>
+                <Route path="/todos/new" exact component={Root} />
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
+
+
+// import React from 'react';
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+// import { NavBar, Footer } from '../components';
+// import { Root, RecipesList, RecipesCreate, RecipesUpdate, RecipeShow, Registration, Message } from '../pages';
+
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+// function App() {
+//     return (
+//         <Router>
+//             <NavBar />
+//             <Switch>
+//                 <Route path="/" exact component={Root} />
+//                 <Route path="/recipes/list" exact component={RecipesList} />
+//                 <Route path="/recipes/create" exact component={RecipesCreate} />
+//                 <Route path="/recipes/update/:id" exact component={RecipesUpdate} />
+//                 <Route path="/recipes/:id" exact component={RecipeShow} />
+//                 <Route path="/users/register" exact component={Registration} />
+//             </Switch>
+//             <Footer />
+//         </Router>
+//     );
+// };
+
+// export default App;
