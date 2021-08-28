9cf1895e5984a762dde5eec16af392cd73c0ae81
Yun Chung <yun.chung@mac.com>
2021-08-28 16:39:42 +0900

    index page useState hooks

--- mern-todo/client/src/pages/todosIndex.jsx
+++ mern-todo/client/src/pages/todosIndex.jsx
@@ -1,6 +1,11 @@
 import { Link } from "react-router-dom";
+import { useState, useEffect } from 'react';
 
 const TodosIndex = () => {
+    const [todos, setTodos] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
+
     return (
         <Link to={'/todos/new'}><button>create a new todo</button></Link>
     );
@@ -14,7 +19,6 @@ build render
 
 
 
-import { useState, useEffect } from 'react';
 
 const useFetch = (url) => {
     const [data, setData] = useState(null);
